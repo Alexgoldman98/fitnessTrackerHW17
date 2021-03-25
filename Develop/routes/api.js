b90c9920a01ceffc544706c0ca37de5544c504da
Alexandra <Alexgoldman98@gmail.com>
2021-03-25 09:19:40 -0400

    created post route in api.js

---
+++ Develop/routes/api.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
